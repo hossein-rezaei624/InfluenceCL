cf5fa58092a4fa87e9fe574d0a1c77a439d9f7ca
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2024-04-05 06:38:40 +0330

    Update ntk_generator.py

--- utils/ntk_generator.py
+++ utils/ntk_generator.py
@@ -2,7 +2,6 @@
 import numpy as np
 from jax import jit
 from neural_tangents import stax
-import jax
 
 _, _, kernel_fn = stax.serial(
     stax.Dense(100, 1., 0.05),
@@ -14,7 +13,6 @@ fnn_kernel_fn = jit(kernel_fn, static_argnums=(2,))
 
 
 def generate_fnn_ntk(X, Y):
-    print("subbbbb333", jax.devices())
     return np.array(fnn_kernel_fn(X, Y, 'ntk'))
 
 
@@ -62,10 +60,8 @@ def generate_resnet_ntk(X, Y, skip=25):
 
 
 def get_kernel_fn(bone):
-    print("subbbbb111", jax.devices())
     from backbone.MNISTMLP import MNISTMLP
     from backbone.ResNet18 import ResNet
-    print("subbbbb222", jax.devices())
     if isinstance(bone, MNISTMLP):
         return lambda x, y: generate_fnn_ntk(x.reshape(-1, 28, 28, 1), y.reshape(-1, 28, 28, 1))
     elif isinstance(bone, ResNet):
