64d99200923d439922aded31405ce9cc2b3d1bb7
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2024-04-05 06:50:23 +0330

    Update soif.py

--- models/soif.py
+++ models/soif.py
@@ -5,7 +5,6 @@ from models.utils.continual_model import ContinualModel
 import numpy as np
 from utils.ntk_generator import get_kernel_fn
 from utils.influence_ntk import InfluenceNTK
-import jax
 
 def get_parser() -> ArgumentParser:
     parser = ArgumentParser(description='Continual learning')
@@ -47,7 +46,6 @@ class SOIF(ContinualModel):
 
     def observe(self, inputs, labels, not_aug_inputs):
         real_batch_size = inputs.shape[0]
-        print("firsttttt", jax.devices())
         self.opt.zero_grad()
         if not self.buffer.is_empty():
             indexes, buf_inputs, buf_labels = self.buffer.get_data(
@@ -70,10 +68,8 @@ class SOIF(ContinualModel):
                 buf_inputs, buf_labels = self.buffer.get_all_data()
                 inputs = torch.cat((inputs[:real_batch_size], buf_inputs))
                 labels = torch.cat((labels[:real_batch_size], buf_labels))
-                print("mainnnnnnn111", jax.devices())
                 chosen_indexes = self.ifs.select(inputs.cpu(), labels.cpu(), self.buffer.buffer_size, self.kernel_fn,
                                                  self.args.lmbda, self.args.mu, self.args.nu, inc_weight)[0]
-                print("mainnnnnnn222", jax.devices())
                 out_indexes = np.setdiff1d(np.arange(self.buffer.buffer_size), chosen_indexes - real_batch_size)
                 in_indexes = chosen_indexes[chosen_indexes < real_batch_size]
                 self.buffer.replace_data(out_indexes, inputs[in_indexes], labels[in_indexes])
