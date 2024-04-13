d2cf761708f25966d5604f833b096c113a572878
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2024-04-13 16:02:23 +0330

    Update casp.py

--- models/casp.py
+++ models/casp.py
@@ -111,8 +111,8 @@ class Casp(ContinualModel):
         #print("unique_classes:", self.unique_classes)
         self.mapping = {value: index for index, value in enumerate(self.unique_classes)}
         self.reverse_mapping = {index: value for value, index in self.mapping.items()}
-        self.confidence_by_class = {class_id: {epoch: [] for epoch in range(20)} for class_id, __ in enumerate(self.unique_classes)}
-        self.confidence_by_sample = torch.zeros((20, self.n_sample_per_task))
+        self.confidence_by_class = {class_id: {epoch: [] for epoch in range(6)} for class_id, __ in enumerate(self.unique_classes)}
+        self.confidence_by_sample = torch.zeros((6, self.n_sample_per_task))
     
     def end_epoch(self, dataset, train_loader):
         self.epoch += 1
@@ -122,7 +122,7 @@ class Casp(ContinualModel):
             mean_by_class = {class_id: {epoch: torch.mean(torch.tensor(confidences[epoch])) for epoch in confidences} for class_id, confidences in self.confidence_by_class.items()}
             
             # Calculate standard deviation of mean confidences by class
-            std_of_means_by_class = {class_id: torch.std(torch.tensor([mean_by_class[class_id][epoch] for epoch in range(20)])) for class_id, __ in enumerate(self.unique_classes)}
+            std_of_means_by_class = {class_id: torch.std(torch.tensor([mean_by_class[class_id][epoch] for epoch in range(6)])) for class_id, __ in enumerate(self.unique_classes)}
             
             # Compute mean and variability of confidences for each sample
             Confidence_mean = self.confidence_by_sample.mean(dim=0)
@@ -248,8 +248,8 @@ class Casp(ContinualModel):
             shuffled_labels = all_labels_[indices]
         
             # Update the buffer with the shuffled images and labels
-            self.buffer.labels[list_of_indices] = shuffled_labels.to(self.device)
-            self.buffer.examples[list_of_indices] = shuffled_images.to(self.device)
+            ##self.buffer.labels[list_of_indices] = shuffled_labels.to(self.device)
+            ##self.buffer.examples[list_of_indices] = shuffled_images.to(self.device)
 
 
     def observe(self, inputs, labels, not_aug_inputs, index_):
@@ -259,7 +259,7 @@ class Casp(ContinualModel):
         
         real_batch_size = inputs.shape[0]
 
-        if self.epoch < 20:
+        if self.epoch < 6:
             targets = torch.tensor([self.mapping[val.item()] for val in labels]).to(self.device)
             confidence_batch = []
 
@@ -278,7 +278,7 @@ class Casp(ContinualModel):
         novel_loss = 0*self.loss(logits, batch_y_combine)
         self.opt.zero_grad()
 
-        if self.epoch < 20:
+        if self.epoch < 6:
             soft_ = soft_1(logits)
             # Accumulate confidences
             for i in range(targets.shape[0]):
