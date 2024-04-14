1fcd3502b9e269d5c187e9eaf2ffa95536d520bd
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2024-04-15 02:37:35 +0330

    Update seq_cifar100.py

--- datasets/seq_cifar100.py
+++ datasets/seq_cifar100.py
@@ -69,7 +69,9 @@ class SequentialCIFAR100(ContinualDataset):
     N_CLASSES_PER_TASK = 10
     N_TASKS = 10
     TRANSFORM = transforms.Compose(
-            [transforms.ToTensor(),
+            [transforms.RandomCrop(32, padding=4),
+             transforms.RandomHorizontalFlip(),
+             transforms.ToTensor(),
              transforms.Normalize((0.5071, 0.4867, 0.4408),
                                   (0.2675, 0.2565, 0.2761))])
 
