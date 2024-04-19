8be346c62515f263fb13cedec3d7217624aa6204
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2024-04-19 23:10:58 +0330

    Update casp.py

--- models/casp.py
+++ models/casp.py
@@ -234,7 +234,8 @@ class Casp(ContinualModel):
 
             self.class_portion.append(updated_std_of_means_by_class)
             self.task_portion.append((self.confidence_by_sample.std(dim=1)).mean(dim=0))
-
+            print("self.class_portion", self.class_portion)
+            print("self.task_portion", self.task_portion)
             
             # Distribute samples based on the standard deviation
             dist = distribute_samples(updated_std_of_means_by_class, top_n)
