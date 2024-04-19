b0040dd3a2f86dae9d3164a7b7ffeec36d7dda85
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2024-04-19 22:05:17 +0330

    Update buffer.py

--- utils/buffer.py
+++ utils/buffer.py
@@ -136,7 +136,6 @@ class Buffer:
         """
         if not hasattr(self, 'examples'):
             self.init_tensors(examples, labels, logits, task_labels)
-            print("self.labelsssssssssss", self.labels)
         
         for i in range(examples.shape[0]):
             index = reservoir(self.num_seen_examples, self.buffer_size)
