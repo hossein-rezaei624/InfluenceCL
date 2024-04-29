f9845adb8c09be3d6ba9bb1643d5553035d67f9b
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2024-04-29 21:36:50 +0330

    Update casp.py

--- models/casp.py
+++ models/casp.py
@@ -189,7 +189,7 @@ class Casp(ContinualModel):
         
         if self.epoch == self.args.n_epochs:
             # Calculate mean confidence by class
-            mean_by_class = {class_id: {epoch: torch.std(torch.tensor(confidences[epoch])) for epoch in confidences} for class_id, confidences in self.confidence_by_class.items()}
+            mean_by_class = {class_id: {epoch: torch.std(torch.tensor(confidences[epoch])) for epoch in range(self.predicted_epoch)} for class_id, confidences in self.confidence_by_class.items()}
             
             # Calculate standard deviation of mean confidences by class
             std_of_means_by_class = {class_id: torch.mean(torch.tensor([mean_by_class[class_id][epoch] for epoch in range(self.predicted_epoch)])) for class_id, __ in enumerate(self.unique_classes)}
@@ -199,6 +199,8 @@ class Casp(ContinualModel):
    ####         std_of_means_by_task = {task_id: torch.mean(torch.tensor([mean_by_task[task_id][epoch] for epoch in range(self.args.casp_epoch)])) for task_id in range(self.task)}
             std_of_means_by_task = {task_id: mean_by_task[task_id] for task_id in range(self.task)}
 
+
+            self.confidence_by_sample = self.confidence_by_sample[:self.predicted_epoch]
             
             # Compute mean and variability of confidences for each sample
             Confidence_mean = self.confidence_by_sample.mean(dim=0)
