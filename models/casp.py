007defa31aa905863a4cf0ba293f641f4788e6e7
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2024-04-30 23:30:58 +0330

    Update casp.py

--- models/casp.py
+++ models/casp.py
@@ -449,15 +449,15 @@ class Casp(ContinualModel):
                 # Update the dictionary with the confidence score for the current class for the current epoch
                 self.confidence_by_class[targets[i].item()][self.epoch].append(soft_[i, labels[i]].item())
 
-                if self.epoch == 0 and self.task == 1:
-
-                    soft_new = soft_[:, list(self.unique_classes)]
-                    
-                    # Calculate the entropy for the i-th prediction
-                    entropy = -torch.sum(soft_new[i] * torch.log(soft_new[i] + 1e-9))  # Adding a small constant to avoid log(0)
-                
-                    # Append the entropy (uncertainty measure) instead of the softmax value of the true class
-                    self.task_conf_first.append(entropy.item())
+###                if self.epoch == 0 and self.task == 1:
+###
+###                    soft_new = soft_[:, list(self.unique_classes)]
+###                    
+###                    # Calculate the entropy for the i-th prediction
+###                    entropy = -torch.sum(soft_new[i] * torch.log(soft_new[i] + 1e-9))  # Adding a small constant to avoid log(0)
+###                
+###                    # Append the entropy (uncertainty measure) instead of the softmax value of the true class
+###                    self.task_conf_first.append(entropy.item())
             
             # Record the confidence scores for samples in the corresponding tensor
             conf_tensor = torch.tensor(confidence_batch)
@@ -516,6 +516,9 @@ class Casp(ContinualModel):
                                      dim=1)
             PSC = SupConLoss(temperature=0.09, contrast_mode='proxy')
             novel_loss += PSC(features=cos_features, labels=combined_labels)
+
+        if self.epoch == 0 and self.task == 1:
+            self.task_conf_first.append(self.loss(casp_logits, labels).item())
 
         novel_loss.backward()
         self.opt.step()
