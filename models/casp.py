6a9031f8a4837cd4361814c4d9756e359fdcf75e
Hossein Rezaei <hr505@cam.ac.uk>
2024-06-17 22:51:52 +0330

    Update casp.py

--- models/casp.py
+++ models/casp.py
@@ -204,11 +204,11 @@ class Casp(ContinualModel):
             
 
             ###self.confidence_by_sample = self.confidence_by_sample[:self.predicted_epoch]
-            self.confidence_by_sample = self.confidence_by_sample[:6]
+            ###self.confidence_by_sample = self.confidence_by_sample[:6]
             
             # Compute mean and variability of confidences for each sample
-            Confidence_mean = self.confidence_by_sample.mean(dim=0)
-            Variability = self.confidence_by_sample.std(dim=0)
+            Confidence_mean = self.confidence_by_sample[:6].mean(dim=0)
+            Variability = self.confidence_by_sample[:6].std(dim=0)
 
             ##plt.scatter(Variability, Confidence_mean, s = 2)
             
@@ -433,7 +433,7 @@ class Casp(ContinualModel):
 
         real_batch_size = inputs.shape[0]
         
-        if self.epoch < self.predicted_epoch: #self.predicted_epoch
+        if self.epoch < 12: #self.predicted_epoch
             targets = torch.tensor([self.mapping[val.item()] for val in labels]).to(self.device)
             confidence_batch = []
 
@@ -451,7 +451,7 @@ class Casp(ContinualModel):
         novel_loss = 0*self.loss(logits, batch_y_combine)
         self.opt.zero_grad()
 
-        if self.epoch < self.predicted_epoch:  #self.predicted_epoch
+        if self.epoch < 12:  #self.predicted_epoch
             casp_logits, _ = self.net.pcrForward(not_aug_inputs)
             soft_ = soft_1(casp_logits)
             # Accumulate confidences
