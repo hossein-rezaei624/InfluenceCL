dd4f4e998fccd7edd203e0ea58a550262677a841
Hossein Rezaei <hr505@cam.ac.uk>
2024-06-20 19:05:25 +0330

    Update casp.py

--- models/casp.py
+++ models/casp.py
@@ -433,7 +433,7 @@ class Casp(ContinualModel):
 
         real_batch_size = inputs.shape[0]
         
-        if self.epoch < 12: #self.predicted_epoch
+        if self.epoch < 11: #self.predicted_epoch
             targets = torch.tensor([self.mapping[val.item()] for val in labels]).to(self.device)
             confidence_batch = []
 
@@ -451,7 +451,7 @@ class Casp(ContinualModel):
         novel_loss = 0*self.loss(logits, batch_y_combine)
         self.opt.zero_grad()
 
-        if self.epoch < 12:  #self.predicted_epoch
+        if self.epoch < 11:  #self.predicted_epoch
             casp_logits, _ = self.net.pcrForward(not_aug_inputs)
             soft_ = soft_1(casp_logits)
             # Accumulate confidences
