8c75026ddf349f7be575fe6c024445ada17dbe09
Hossein Rezaei <hr505@cam.ac.uk>
2024-06-23 18:51:56 +0330

    Update casp.py

--- models/casp.py
+++ models/casp.py
@@ -204,8 +204,8 @@ class Casp(ContinualModel):
             
             
             # Compute mean and variability of confidences for each sample
-            Confidence_mean = self.confidence_by_sample[:5].mean(dim=0)
-            Variability = self.confidence_by_sample[:5].var(dim=0)
+            Confidence_mean = self.confidence_by_sample[:self.predicted_epoch].mean(dim=0)
+            Variability = self.confidence_by_sample[:self.predicted_epoch].var(dim=0)
 
             ##plt.scatter(Variability, Confidence_mean, s = 2)
             
@@ -431,7 +431,7 @@ class Casp(ContinualModel):
 
         real_batch_size = inputs.shape[0]
         
-        if self.epoch < 11: #self.predicted_epoch
+        if self.epoch < self.predicted_epoch: #self.predicted_epoch
             targets = torch.tensor([self.mapping[val.item()] for val in labels]).to(self.device)
             confidence_batch = []
 
@@ -449,7 +449,7 @@ class Casp(ContinualModel):
         novel_loss = 0*self.loss(logits, batch_y_combine)
         self.opt.zero_grad()
 
-        if self.epoch < 11:  #self.predicted_epoch
+        if self.epoch < self.predicted_epoch:  #self.predicted_epoch
             self.net.eval()
             with torch.no_grad():
                 casp_logits, _ = self.net.pcrForward(not_aug_inputs)
