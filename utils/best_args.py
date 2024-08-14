4a7fcde7907532c1e430724d0aac427e29c70130
Hossein Rezaei <hr505@cam.ac.uk>
2024-08-15 00:55:02 +0330

    Update best_args.py

--- utils/best_args.py
+++ utils/best_args.py
@@ -563,6 +563,7 @@ best_args = {
                                   'n_epochs': 50},
                             500: {'lr': 0.03, 'gamma': 0.5,
                                   'batch_size': 32,
+                                  'minibatch_size': 32,
                                   'n_epochs': 50},
                             1000: {'lr': 0.03, 'gamma': 0.5,
                                    'batch_size': 32,
