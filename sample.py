ff6166da7bdb9ea4d929776b5f3d93433f5910a5
Hossein Rezaei <hr505@cam.ac.uk>
2024-08-26 01:20:36 +0330

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,51 +1,34 @@
-# Copyright 2022-present, Lorenzo Bonicelli, Pietro Buzzega, Matteo Boschini, Angelo Porrello, Simone Calderara.
+# Copyright 2020-present, Pietro Buzzega, Matteo Boschini, Angelo Porrello, Simone Calderara.
 # All rights reserved.
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
-
-from typing import Tuple
-
-import torch.nn.functional as F
-import torch.optim
-import torchvision.transforms as transforms
-from backbone.ResNet18 import resnet18
-from PIL import Image
+from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR100
-
-from datasets.transforms.denormalization import DeNormalize
-from datasets.utils.continual_dataset import (ContinualDataset,
-                                              store_masked_loaders)
+import torchvision.transforms as transforms
+from backbone.ResNet import ResNet18
+import torch.nn.functional as F
+from utils.conf import base_path
+from PIL import Image
 from datasets.utils.validation import get_train_val
-from utils.conf import base_path_dataset as base_path
-
-
-class TCIFAR100(CIFAR100):
-    """Workaround to avoid printing the already downloaded messages."""
-    def __init__(self, root, train=True, transform=None,
-                 target_transform=None, download=False) -> None:
-        self.root = root
-        super(TCIFAR100, self).__init__(root, train, transform, target_transform, download=not self._check_integrity())
+from datasets.utils.continual_dataset import ContinualDataset, store_masked_loaders
+from datasets.utils.continual_dataset import get_previous_train_loader
+from datasets.transforms.denormalization import DeNormalize
+import torch
+from typing import Tuple
+import numpy as np
 
 class MyCIFAR100(CIFAR100):
-    """
-    Overrides the CIFAR100 dataset to change the getitem function.
-    """
-    def __init__(self, root, train=True, transform=None,
-                 target_transform=None, download=False) -> None:
-        self.not_aug_transform = transforms.Compose([transforms.ToTensor()])
-        self.root = root
-        super(MyCIFAR100, self).__init__(root, train, transform, target_transform, not self._check_integrity())
 
-    def __getitem__(self, index: int) -> Tuple[Image.Image, int, Image.Image]:
-        """
-        Gets the requested element from the dataset.
-        :param index: index of the element to be returned
-        :returns: tuple: (image, target) where target is index of the target class.
-        """
+    def __init__(self, root, train=True, transform=None, target_transform=None, download=False):
+        self.not_aug_transform = transforms.Compose([transforms.ToTensor()])
+        super(MyCIFAR100, self).__init__(root, train, transform, target_transform, download)
+
+    def __getitem__(self, index):
         img, target = self.data[index], self.targets[index]
 
+        # doing this so that it is consistent with all other datasets
         # to return a PIL Image
-        img = Image.fromarray(img, mode='RGB')
+        img = Image.fromarray(img)
         original_img = img.copy()
 
         not_aug_img = self.not_aug_transform(original_img)
@@ -59,7 +42,7 @@ class MyCIFAR100(CIFAR100):
         if hasattr(self, 'logits'):
             return img, target, not_aug_img, self.logits[index]
 
-        return img, target, not_aug_img, index
+        return img, target, not_aug_img
 
 
 class SequentialCIFAR100(ContinualDataset):
@@ -72,44 +55,75 @@ class SequentialCIFAR100(ContinualDataset):
             [transforms.RandomCrop(32, padding=4),
              transforms.RandomHorizontalFlip(),
              transforms.ToTensor(),
-             transforms.Normalize((0.5071, 0.4867, 0.4408),
-                                  (0.2675, 0.2565, 0.2761))])
+             transforms.Normalize((0.5071, 0.4865, 0.4409),
+                                  (0.2673, 0.2564, 0.2761))])
 
-    def get_examples_number(self):
-        train_dataset = MyCIFAR100(base_path() + 'CIFAR100', train=True,
-                                  download=True)
-        return len(train_dataset.data)
-
-    def get_data_loaders(self):
+    def get_data_loaders(self, nomask=False):
         transform = self.TRANSFORM
 
         test_transform = transforms.Compose(
             [transforms.ToTensor(), self.get_normalization_transform()])
 
         train_dataset = MyCIFAR100(base_path() + 'CIFAR100', train=True,
-                                  download=True, transform=transform)
-        train_dataset.not_aug_transform = test_transform  # store normalized images in the buffer
+                                   download=True, transform=transform)
         if self.args.validation:
             train_dataset, test_dataset = get_train_val(train_dataset,
                                                     test_transform, self.NAME)
         else:
-            test_dataset = TCIFAR100(base_path() + 'CIFAR100',train=False,
-                                   download=True, transform=test_transform)
+            test_dataset = CIFAR100(base_path() + 'CIFAR100', train=False,
+                                    download=True, transform=test_transform)
 
-        train, test = store_masked_loaders(train_dataset, test_dataset, self)
+        if not nomask:
+            if isinstance(train_dataset.targets, list):
+                train_dataset.targets = torch.tensor(train_dataset.targets, dtype=torch.long)
+            if isinstance(test_dataset.targets, list):
+                test_dataset.targets = torch.tensor(test_dataset.targets, dtype=torch.long)
+            train, test = store_masked_loaders(train_dataset, test_dataset, self)
+            return train, test
+        else:
+            train_loader = DataLoader(train_dataset,
+                                      batch_size=32, shuffle=True, num_workers=2)
+            test_loader = DataLoader(test_dataset,
+                                     batch_size=32, shuffle=False, num_workers=2)
+            return train_loader, test_loader
 
-        return train, test
+    def get_joint_loaders(self, nomask=False):
+        return self.get_data_loaders(nomask=True)
+
+    def not_aug_dataloader(self, args, batch_size):
+
+        if hasattr(args, 'iba') and args.iba:
+            transform = transforms.Compose([transforms.ToTensor()])
+        else:
+            transform = transforms.Compose([transforms.ToTensor(),
+                                            self.get_normalization_transform()])
+
+        train_dataset = MyCIFAR100(base_path() + 'CIFAR100', train=True,
+                                  download=True, transform=transform)
+
+        if isinstance(train_dataset.targets, list):
+            train_dataset.targets = torch.tensor(train_dataset.targets, dtype=torch.long)
+
+        train_mask = np.logical_and(np.array(train_dataset.targets) >= (self.i - 1) * self.N_CLASSES_PER_TASK
+                                    , np.array(train_dataset.targets) < self.i * self.N_CLASSES_PER_TASK)
+
+        train_dataset.data = train_dataset.data[train_mask]
+        train_dataset.targets = np.array(train_dataset.targets)[train_mask]
+
+        train_loader = get_previous_train_loader(train_dataset, batch_size, self)
+
+        return train_loader
 
     @staticmethod
     def get_transform():
         transform = transforms.Compose(
-            [transforms.RandomCrop(32, padding=4), transforms.RandomHorizontalFlip()])
+            [transforms.ToPILImage(), SequentialCIFAR100.TRANSFORM])
         return transform
 
     @staticmethod
-    def get_backbone(args):
-        return resnet18(SequentialCIFAR100.N_CLASSES_PER_TASK
-                        * SequentialCIFAR100.N_TASKS, args)
+    def get_backbone():
+        return ResNet18(SequentialCIFAR100.N_CLASSES_PER_TASK
+                        * SequentialCIFAR100.N_TASKS)
 
     @staticmethod
     def get_loss():
@@ -117,28 +131,10 @@ class SequentialCIFAR100(ContinualDataset):
 
     @staticmethod
     def get_normalization_transform():
-        transform = transforms.Normalize((0.5071, 0.4867, 0.4408),
-                                         (0.2675, 0.2565, 0.2761))
+        transform = transforms.Normalize((0.5071, 0.4865, 0.4409), (0.2673, 0.2564, 0.2761))
         return transform
 
     @staticmethod
     def get_denormalization_transform():
-        transform = DeNormalize((0.5071, 0.4867, 0.4408),
-                                (0.2675, 0.2565, 0.2761))
+        transform = DeNormalize((0.5071, 0.4865, 0.4409), (0.2673, 0.2564, 0.2761))
         return transform
-
-    @staticmethod
-    def get_epochs():
-        return 50
-
-    @staticmethod
-    def get_batch_size():
-        return 32
-
-    @staticmethod
-    def get_minibatch_size():
-        return SequentialCIFAR100.get_batch_size()
-
-    @staticmethod
-    def get_scheduler(model, args):
-        return None
