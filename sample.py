7b7e4bc8857d4854346c4db253dc632b774b623c
Hossein Rezaei <hr505@cam.ac.uk>
2024-08-26 15:40:47 +0330

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,8 +1,8 @@
-# Copyright 2022-present, Lorenzo Bonicelli, Pietro Buzzega, Matteo Boschini, Angelo Porrello, Simone Calderara.
+# Copyright 2020-present, Pietro Buzzega, Matteo Boschini, Angelo Porrello, Simone Calderara.
 # All rights reserved.
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
-
+from abc import abstractmethod
 from argparse import Namespace
 from torch import nn as nn
 from torchvision.transforms import transforms
@@ -10,8 +10,7 @@ from torch.utils.data import DataLoader
 from typing import Tuple
 from torchvision import datasets
 import numpy as np
-import torch.optim
-
+import torch
 
 class ContinualDataset:
     """
@@ -33,71 +32,61 @@ class ContinualDataset:
         self.i = 0
         self.args = args
 
+    @abstractmethod
     def get_data_loaders(self) -> Tuple[DataLoader, DataLoader]:
         """
         Creates and returns the training and test loaders for the current task.
         The current training loader and all test loaders are stored in self.
         :return: the current training and test loaders
         """
-        raise NotImplementedError
+        pass
+
+    @abstractmethod
+    def not_aug_dataloader(self, args: Namespace, batch_size: int) -> DataLoader:
+        """
+        Returns the dataloader of the current task,
+        not applying data augmentation.
+        :param args: the parsed arguments
+        :param batch_size: the batch size of the loader
+        :return: the current training loader
+        """
+        pass
 
     @staticmethod
+    @abstractmethod
     def get_backbone() -> nn.Module:
         """
         Returns the backbone to be used for to the current dataset.
         """
-        raise NotImplementedError
+        pass
 
     @staticmethod
+    @abstractmethod
     def get_transform() -> transforms:
         """
         Returns the transform to be used for to the current dataset.
         """
-        raise NotImplementedError
+        pass
 
     @staticmethod
+    @abstractmethod
     def get_loss() -> nn.functional:
         """
         Returns the loss to be used for to the current dataset.
         """
-        raise NotImplementedError
+        pass
 
     @staticmethod
+    @abstractmethod
     def get_normalization_transform() -> transforms:
         """
         Returns the transform used for normalizing the current dataset.
         """
-        raise NotImplementedError
-
-    @staticmethod
-    def get_denormalization_transform() -> transforms:
-        """
-        Returns the transform used for denormalizing the current dataset.
-        """
-        raise NotImplementedError
-
-    @staticmethod
-    def get_scheduler(model, args: Namespace) -> torch.optim.lr_scheduler:
-        """
-        Returns the scheduler to be used for to the current dataset.
-        """
-        raise NotImplementedError
-
-    @staticmethod
-    def get_epochs():
-        raise NotImplementedError
-
-    @staticmethod
-    def get_batch_size():
-        raise NotImplementedError
-
-    @staticmethod
-    def get_minibatch_size():
-        raise NotImplementedError
+        pass
 
 
 def store_masked_loaders(train_dataset: datasets, test_dataset: datasets,
-                         setting: ContinualDataset) -> Tuple[DataLoader, DataLoader]:
+                    setting: ContinualDataset) -> Tuple[DataLoader, DataLoader]:
     """
     Divides the dataset into tasks.
     :param train_dataset: train dataset
@@ -105,10 +94,19 @@ def store_masked_loaders(train_dataset: datasets, test_dataset: datasets,
     :param setting: continual learning setting
     :return: train and test loaders
     """
-    train_mask = np.logical_and(np.array(train_dataset.targets) >= setting.i,
-                                np.array(train_dataset.targets) < setting.i + setting.N_CLASSES_PER_TASK)
-    test_mask = np.logical_and(np.array(test_dataset.targets) >= setting.i,
-                               np.array(test_dataset.targets) < setting.i + setting.N_CLASSES_PER_TASK)
+    # core50 (i.e. uneven # of classes)
+    if type(setting.N_CLASSES_PER_TASK) == list:
+        FROM_CLASS = np.sum(setting.N_CLASSES_PER_TASK[:setting.i])
+        TO_CLASS = np.sum(setting.N_CLASSES_PER_TASK[:setting.i+1])
+    # any other dataset
+    else:
+        FROM_CLASS = setting.i * setting.N_CLASSES_PER_TASK
+        TO_CLASS = (setting.i + 1) * setting.N_CLASSES_PER_TASK
+
+    train_mask = np.logical_and(np.array(train_dataset.targets % 1000) >= FROM_CLASS,
+        np.array(train_dataset.targets % 1000) < TO_CLASS)
+    test_mask = np.logical_and(np.array(test_dataset.targets % 1000) >= FROM_CLASS,
+        np.array(test_dataset.targets % 1000) < TO_CLASS)
 
     train_dataset.data = train_dataset.data[train_mask]
     test_dataset.data = test_dataset.data[test_mask]
@@ -117,13 +115,13 @@ def store_masked_loaders(train_dataset: datasets, test_dataset: datasets,
     test_dataset.targets = np.array(test_dataset.targets)[test_mask]
 
     train_loader = DataLoader(train_dataset,
-                              batch_size=setting.args.batch_size, shuffle=True, num_workers=0)
+                              batch_size=setting.args.batch_size, shuffle=True, num_workers=2)
     test_loader = DataLoader(test_dataset,
-                             batch_size=setting.args.batch_size, shuffle=False, num_workers=0)
+                             batch_size=setting.args.batch_size, shuffle=False, num_workers=2)
     setting.test_loaders.append(test_loader)
     setting.train_loader = train_loader
 
-    setting.i += setting.N_CLASSES_PER_TASK
+    setting.i += 1
     return train_loader, test_loader
 
 
@@ -136,11 +134,20 @@ def get_previous_train_loader(train_dataset: datasets, batch_size: int,
     :param setting: the continual dataset at hand
     :return: a dataloader
     """
+    # core50 (i.e. uneven # of classes)
+    if type(setting.N_CLASSES_PER_TASK) == list:
+        FROM_CLASS = np.sum(setting.N_CLASSES_PER_TASK[:setting.i - 1])
+        TO_CLASS = np.sum(setting.N_CLASSES_PER_TASK[:setting.i])
+    # any other dataset
+    else:
+        FROM_CLASS = (setting.i - 1) * setting.N_CLASSES_PER_TASK
+        TO_CLASS = (setting.i) * setting.N_CLASSES_PER_TASK
+
     train_mask = np.logical_and(np.array(train_dataset.targets) >=
-                                setting.i - setting.N_CLASSES_PER_TASK, np.array(train_dataset.targets)
-                                < setting.i - setting.N_CLASSES_PER_TASK + setting.N_CLASSES_PER_TASK)
+        FROM_CLASS, np.array(train_dataset.targets)
+        < TO_CLASS)
 
     train_dataset.data = train_dataset.data[train_mask]
     train_dataset.targets = np.array(train_dataset.targets)[train_mask]
 
-    return DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    return DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
