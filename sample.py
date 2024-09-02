8c598866f28612513b446dd9fdf2c7e5b09c9708
Hossein Rezaei <hr505@cam.ac.uk>
2024-09-02 17:40:00 +0330

    Update sample.py

--- sample.py
+++ sample.py
@@ -11,10 +11,12 @@ import torch.nn as nn
 class SupConLoss(nn.Module):
     """Supervised Contrastive Learning: https://arxiv.org/pdf/2004.11362.pdf.
     It also supports the unsupervised contrastive loss in SimCLR"""
-    def __init__(self, temperature=0.07, contrast_mode='all'):
+    def __init__(self, temperature=0.07, contrast_mode='all',
+                 base_temperature=0.07):
         super(SupConLoss, self).__init__()
         self.temperature = temperature
         self.contrast_mode = contrast_mode
+        self.base_temperature = base_temperature
 
     def forward(self, features, labels=None, mask=None):
         """Compute loss for model. If both `labels` and `mask` are None,
@@ -60,11 +62,6 @@ class SupConLoss(nn.Module):
         elif self.contrast_mode == 'all':
             anchor_feature = contrast_feature
             anchor_count = contrast_count
-        elif self.contrast_mode == 'proxy':
-            anchor_feature = features[:, 0]
-            contrast_feature = features[:, 1]
-            anchor_count = 1
-            contrast_count = 1
         else:
             raise ValueError('Unknown mode: {}'.format(self.contrast_mode))
 
@@ -85,20 +82,25 @@ class SupConLoss(nn.Module):
             torch.arange(batch_size * anchor_count).view(-1, 1).to(device),
             0
         )
+        mask = mask * logits_mask
 
         # compute log_prob
-        if self.contrast_mode == 'proxy':
-            exp_logits = torch.exp(logits)
-        else:
-            mask = mask * logits_mask
-            exp_logits = torch.exp(logits) * logits_mask
+        exp_logits = torch.exp(logits) * logits_mask
         log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
 
         # compute mean of log-likelihood over positive
-        mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
+        # modified to handle edge cases when there is no positive pair
+        # for an anchor point. 
+        # Edge case e.g.:- 
+        # features of shape: [4,1,...]
+        # labels:            [0,1,1,2]
+        # loss before mean:  [nan, ..., ..., nan] 
+        mask_pos_pairs = mask.sum(1)
+        mask_pos_pairs = torch.where(mask_pos_pairs < 1e-6, 1, mask_pos_pairs)
+        mean_log_prob_pos = (mask * log_prob).sum(1) / mask_pos_pairs
 
         # loss
-        loss = -1 * mean_log_prob_pos
+        loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
         loss = loss.view(anchor_count, batch_size).mean()
 
         return loss
