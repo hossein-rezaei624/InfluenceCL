ab67fcd66b2ab1acabc8e62b090e596240f85b7c
Hossein Rezaei <hr505@cam.ac.uk>
2024-09-02 16:39:23 +0330

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,8 +1,8 @@
 """
 Author: Yonglong Tian (yonglong@mit.edu)
 Date: May 07, 2020
-Source: https://github.com/HobbitLong/SupContrast/blob/master/losses.py
 """
+from __future__ import print_function
 
 import torch
 import torch.nn as nn
@@ -11,18 +11,16 @@ import torch.nn as nn
 class SupConLoss(nn.Module):
     """Supervised Contrastive Learning: https://arxiv.org/pdf/2004.11362.pdf.
     It also supports the unsupervised contrastive loss in SimCLR"""
-    def __init__(self, temperature=0.07, contrast_mode='all',
-                 base_temperature=0.07, reduction='mean'):
+    def __init__(self, temperature=0.07, contrast_mode='all'):
         super(SupConLoss, self).__init__()
         self.temperature = temperature
         self.contrast_mode = contrast_mode
-        self.base_temperature = base_temperature
-        self.reduction = reduction
 
     def forward(self, features, labels=None, mask=None):
         """Compute loss for model. If both `labels` and `mask` are None,
         it degenerates to SimCLR unsupervised loss:
         https://arxiv.org/pdf/2002.05709.pdf
+
         Args:
             features: hidden vector of shape [bsz, n_views, ...].
             labels: ground truth of shape [bsz].
@@ -31,7 +29,9 @@ class SupConLoss(nn.Module):
         Returns:
             A loss scalar.
         """
-        device = features.device
+        device = (torch.device('cuda')
+                  if features.is_cuda
+                  else torch.device('cpu'))
 
         if len(features.shape) < 3:
             raise ValueError('`features` needs to be [bsz, n_views, ...],'
@@ -60,6 +60,11 @@ class SupConLoss(nn.Module):
         elif self.contrast_mode == 'all':
             anchor_feature = contrast_feature
             anchor_count = contrast_count
+        elif self.contrast_mode == 'proxy':
+            anchor_feature = features[:, 0]
+            contrast_feature = features[:, 1]
+            anchor_count = 1
+            contrast_count = 1
         else:
             raise ValueError('Unknown mode: {}'.format(self.contrast_mode))
 
@@ -80,17 +85,20 @@ class SupConLoss(nn.Module):
             torch.arange(batch_size * anchor_count).view(-1, 1).to(device),
             0
         )
-        mask = mask * logits_mask
 
         # compute log_prob
-        exp_logits = torch.exp(logits) * logits_mask
+        if self.contrast_mode == 'proxy':
+            exp_logits = torch.exp(logits)
+        else:
+            mask = mask * logits_mask
+            exp_logits = torch.exp(logits) * logits_mask
         log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
 
         # compute mean of log-likelihood over positive
         mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
 
         # loss
-        loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
-        loss = loss.view(anchor_count, batch_size).mean(0)
+        loss = -1 * mean_log_prob_pos
+        loss = loss.view(anchor_count, batch_size).mean()
 
-        return loss.mean() if self.reduction == 'mean' else loss.sum()
+        return loss
