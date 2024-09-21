9880f236e84e90e9f32c8aa945272e07392bffda
Hossein Rezaei <hr505@cam.ac.uk>
2024-09-21 04:08:14 +0330

    add buffer

--- utils/training.py
+++ utils/training.py
@@ -1,4 +1,4 @@
-# Copyright 2022-present, Lorenzo Bonicelli, Pietro Buzzega, Matteo Boschini, Angelo Porrello, Simone Calderara.
+# Copyright 2022-present, Lorenzo Bonicelli, Pietro Buzzega, Matteo Boschini, Angelo Porrello, Simone Calderara.#
 # All rights reserved.
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
