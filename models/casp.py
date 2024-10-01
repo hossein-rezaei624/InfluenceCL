a2925c686ed40fb2379fa9b426f6402acf4aefd0
Hossein Rezaei <hr505@cam.ac.uk>
2024-10-02 03:23:28 +0330

    Update casp.py

--- models/casp.py
+++ models/casp.py
@@ -217,7 +217,7 @@ class Casp(ContinualModel):
 
             
             # Convert standard deviation of means by class to item form
-            updated_std_of_means_by_class = {self.reverse_mapping[k]: 1 for k, _ in updated_std_of_means_by_class.items()}   #uncomment for balance
+            updated_std_of_means_by_class = {self.reverse_mapping[k]: 1 for k, _ in std_of_means_by_class.items()}   #uncomment for balance
             print("updated_std_of_means_by_class", updated_std_of_means_by_class)
 
             self.class_portion.append(updated_std_of_means_by_class)
