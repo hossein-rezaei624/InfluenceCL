801a882755a1f401aa34dd0577b656d69c4d600d
Hossein Rezaei <hr505@cam.ac.uk>
2024-11-03 00:05:14 +0330

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,3 +1,4 @@
+
 Successful companies use advertisements to make more sales. 
     What can make an advertisement effective? 
     Do you think advertisements are a bad thing or a good thing for society?
@@ -9,3 +10,4 @@ Two main approaches that make an advertisement appropriate are targeting the rig
 On the other hand, advertisements can have both positive and negative impacts on the populace. On the positive side, they can help us to find products or services we have already been searching for. To illustrate, consider a crucial tool for your career that you have been unable to find in stores. Suddenly, an online shop advertisement appears on YouTube, allowing you to easily order it. In contrast, advertisements can also have negative effects. When individuals are bombarded and overwhelmed by tons of commercials, consumerism may increase. For instance, a catchy ad on television promoting fast food that lacks sufficient nutrition can encourage unhealthy diets.
 
 To sum up, considering the targeted group of a product and also ensuring the quality of commercials enables companies to purchase more. On the other side, while advertising can bring some advantages—such as facilitating the process of finding a product—it may advance consumerism and encourage poor nutritious diets.
+
